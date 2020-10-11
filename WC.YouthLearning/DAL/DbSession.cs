ea7bf788e31494c66e5d6b7662f4a766b178683f
fangzhen wu <wushengyi@outlook.com>
2020-10-11 19:42:52 +0800

    dal 0.2

---
+++ WC.YouthLearning/DAL/DbSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WC.YouthLearning.DAL
+{
+    public class DbSession : IDbSession
+    {
+        public int SaveChanges()
+        {
+            return DbContentFactory.GetCurrentDbContent().SaveChanges();
+        }
+    }
+}
