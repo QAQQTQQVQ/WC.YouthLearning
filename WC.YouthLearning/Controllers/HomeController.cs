687926a061f030a36d77b581207f1e0b7dea27a6
454313500@qq.com <454313500@qq.com>
2020-10-11 18:55:41 +0800

    first commit

---
+++ WC.YouthLearning/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WC.YouthLearning.Models;
+
+namespace WC.YouthLearning.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext db;
+        public HomeController(DataContext _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+         string name=db.student.First().name;
+
+        ViewBag.Name = name;
+         return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+      
+    }
+}
---
+++ WC.YouthLearning/Models/student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WC.YouthLearning.Models
+{
+    public class student
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Display(Name = "学生姓名")]
+        public string name { get; set; }
+
+        [Display(Name = "学生学号")]
+        public int sid { get; set; }
+
+        [Display(Name = "发表时间")]
+        public string time { get; set; }
+
+        [Display(Name = "次数")]
+        public int num { get; set; }
+
+        [Display(Name = "是否提交")]
+        public int sub { get; set; }
+    }
+}
