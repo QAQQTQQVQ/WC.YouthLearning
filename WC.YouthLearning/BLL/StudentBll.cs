acadb9b8fe0d9ad2a36718472b0b7275a894563f
454313500@qq.com <454313500@qq.com>
2020-10-11 22:11:30 +0800

    test 1

--- WC.YouthLearning/BLL/StudentBll.cs
+++ WC.YouthLearning/BLL/StudentBll.cs
@@ -11,7 +11,7 @@ namespace WC.YouthLearning.BLL
     {
         public StudentBll(BaseDal<student> cd):base(cd)
         {
-            CurrentDal = cd;//666
+            CurrentDal = cd;//666777
         }
 
         public int DeleteListByLogical(List<int> ids)
