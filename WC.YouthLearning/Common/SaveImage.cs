3aa0e19657d6fcfc7baccd05bcd03aa3b1c0c8f2
fangzhen wu <wushengyi@outlook.com>
2020-10-12 17:53:22 +0800

    图片验证

--- WC.YouthLearning/Common/SaveImage.cs
+++ WC.YouthLearning/Common/SaveImage.cs
@@ -9,14 +9,14 @@ namespace WC.YouthLearning.Common
 {
     public static class SaveImage
     {
-        public static bool ByStringToSave(string iss)
+        public static bool ByStringToSave(string name,string iss)
         {
             iss = iss.Replace("data:image/png;base64,", "").Replace("data:image/jgp;base64,", "")
                 .Replace("data:image/jpg;base64,", "").Replace("data:image/jpeg;base64,", "");
             byte[] arr = Convert.FromBase64String(iss);
             MemoryStream ms = new MemoryStream(arr);
             Bitmap bmp = new Bitmap(ms);
-            bmp.Save(@"./wwwroot/StudentImage/test.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            bmp.Save(@"./wwwroot/StudentImage/"+name+".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
             ms.Close();
             return true;
         }
--- WC.YouthLearning/Controllers/UploadImage.cs
+++ WC.YouthLearning/Controllers/UploadImage.cs
@@ -4,24 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WC.YouthLearning.BLL;
 
 namespace WC.YouthLearning.Controllers
 {
     public class UploadImage : Controller
     {
+        private IStudentBll studentBll;
+        public UploadImage(IStudentBll _studentBll)
+        {
+            studentBll = _studentBll;
+        }
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index(string myimg)
+        public IActionResult Index(string name,string myimg)
         {
-            Common.SaveImage.ByStringToSave(myimg);
-            
+            var stu = studentBll.GetEntities(u => u.name == name).FirstOrDefault();
+            if(stu!=null)
+            {
+                Common.SaveImage.ByStringToSave(name, myimg);
+                return Json("提交成功");
+            }
+            else
+            {
+                return Json("未找到该用户");
+            }
 
-
-            return View(model: myimg);
-            return View();
         }
     }
 }
