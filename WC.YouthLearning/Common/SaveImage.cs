15709bd604ff2f1ed8c31b8a1624a72208585f62
fangzhen wu <wushengyi@outlook.com>
2020-10-12 11:27:55 +0800

    image

---
+++ WC.YouthLearning/Common/SaveImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace WC.YouthLearning.Common
+{
+    public static class SaveImage
+    {
+        public static bool ByStringToSave(string iss)
+        {
+            iss = iss.Replace("data:image/png;base64,", "").Replace("data:image/jgp;base64,", "")
+                .Replace("data:image/jpg;base64,", "").Replace("data:image/jpeg;base64,", "");
+            byte[] arr = Convert.FromBase64String(iss);
+            MemoryStream ms = new MemoryStream(arr);
+            Bitmap bmp = new Bitmap(ms);
+            bmp.Save(@"~/StudentImage/test.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            ms.Close();
+            return true;
+        }
+    }
+}
--- WC.YouthLearning/Controllers/UploadImage.cs
+++ WC.YouthLearning/Controllers/UploadImage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,16 @@ namespace WC.YouthLearning.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string myimg)
+        {
+            Common.SaveImage.ByStringToSave(myimg);
+            
+
+
+            return View(model: myimg);
             return View();
         }
     }
